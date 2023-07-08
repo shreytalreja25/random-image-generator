d23f3ed330c37dcdeeccaf42e59252334f63fb58
Shrey Talreja <shreytalreja25@gmail.com>
2023-07-08 16:11:02 +0530

    added react-helmet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import ImageDisplay from './components/ImageDisplay';
 import ShareButton from './components/ShareButton';
+import { Helmet } from 'react-helmet';
 
-function App() {
+const App = () => {
+  const [imageURL, setImageURL] = useState('');
+
+  useEffect(() => {
+    fetchRandomImage();
+  }, []);
+
+  const fetchRandomImage = async () => {
+    try {
+      const response = await fetch('https://random.imagecdn.app/500/500');
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+      setImageURL(url);
+    } catch (error) {
+      console.error('Error fetching random image:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <ImageDisplay />
+      <Helmet>
+        {imageURL && <meta property="og:image" content={imageURL} />}
+      </Helmet>
+
+      <ImageDisplay imageURL={imageURL} />
       <ShareButton />
     </div>
   );
-}
+};
 
 export default App;
