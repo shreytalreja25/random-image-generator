a2bd2cdc1f62720add2c1f577b6cb06f839f0037
Shrey Talreja <shreytalreja25@gmail.com>
2023-07-08 15:43:59 +0530

    Implemented using react share

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -1,41 +1,20 @@
 import React from 'react';
+import { FacebookShareButton, TwitterShareButton, WhatsappShareButton } from 'react-share';
 
 const ShareButton = ({ imageSrc }) => {
-  const handleShareClick = (platform) => {
-    let shareURL = '';
-
-    // Set the share URL based on the selected platform
-    switch (platform) {
-      case 'facebook':
-        shareURL = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-          window.location.href
-        )}`;
-        break;
-      case 'twitter':
-        shareURL = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
-          window.location.href
-        )}`;
-        break;
-      case 'whatsapp':
-        shareURL = `https://api.whatsapp.com/send?text=${encodeURIComponent(
-          window.location.href
-        )}`;
-        break;
-      default:
-        break;
-    }
-
-    // Open the share URL in a new tab
-    if (shareURL) {
-      window.open(shareURL, '_blank');
-    }
-  };
+  const shareURL = window.location.href;
 
   return (
     <div className="share-button">
-      <button onClick={handleShareClick.bind(null, 'facebook')}>Facebook</button>
-      <button onClick={handleShareClick.bind(null, 'twitter')}>Twitter</button>
-      <button onClick={handleShareClick.bind(null, 'whatsapp')}>WhatsApp</button>
+      <FacebookShareButton url={shareURL}>
+        Facebook
+      </FacebookShareButton>
+      <TwitterShareButton url={shareURL}>
+        Twitter
+      </TwitterShareButton>
+      <WhatsappShareButton url={shareURL}>
+        WhatsApp
+      </WhatsappShareButton>
     </div>
   );
 };
