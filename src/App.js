d0ee263703819c3f44bf4be9c4ff34fad15343bb
Shrey Talreja <shreytalreja25@gmail.com>
2023-07-09 15:12:29 +0530

    changed api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ImageDisplay from './components/ImageDisplay';
-import ShareButton from './components/ShareButton';
+
 
 const App = () => {
   return (
     <div className="App">
       <ImageDisplay />
-      <ShareButton />
     </div>
   );
 };
--- src/components/ImageDisplay.js
+++ src/components/ImageDisplay.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import ShareButton from './ShareButton';
+import '../App.css';
 
 const ImageDisplay = () => {
   const [imageURL, setImageURL] = useState('');
@@ -10,22 +13,36 @@ const ImageDisplay = () => {
 
   const fetchRandomImage = async () => {
     try {
-      const response = await fetch('https://random.imagecdn.app/500/500');
-      const blob = await response.blob();
-      const url = URL.createObjectURL(blob);
-      setImageURL(url);
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await response.json();
+
+      if (data.status === 'success') {
+        setImageURL(data.message);
+      }
       setIsLoading(false);
+
+      // Update the og:image meta tag
+      Helmet.canUseDOM && Helmet.peek().metaTags.push({
+        property: 'og:image',
+        content: data.message,
+      });
     } catch (error) {
       console.error('Error fetching random image:', error);
     }
   };
 
   return (
-    <div className="image-display">
+    <div className="App">
+      <Helmet>
+        <meta property="og:image" content="" />
+      </Helmet>
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-        imageURL && <img src={imageURL} alt="Random Pic Generator" />
+        <div>
+          <img src={imageURL} alt="Random Dog Pic" />
+          <ShareButton imageURL={imageURL} />
+        </div>
       )}
     </div>
   );
