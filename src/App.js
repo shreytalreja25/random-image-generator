76137d12bc3915d3db4eed3a8d435242b5733875
Shrey Talreja <shreytalreja25@gmail.com>
2023-07-09 14:56:57 +0530

    fixed share react issue

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import ImageDisplay from './components/ImageDisplay';
 import ShareButton from './components/ShareButton';
-import './App.css'
 
-const ImageDisplay = () => {
-  const [imageURL, setImageURL] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetchRandomImage();
-  }, []);
-
-  const fetchRandomImage = async () => {
-    try {
-      const response = await fetch('https://random.imagecdn.app/500/500');
-      const blob = await response.blob();
-      const url = URL.createObjectURL(blob);
-      setImageURL(url);
-      setIsLoading(false);
-    } catch (error) {
-      console.error('Error fetching random image:', error);
-    }
-  };
-
+const App = () => {
   return (
     <div className="App">
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <div>
-          <img src={imageURL} alt="Random Pic Generator" />
-          <ShareButton imageURL={imageURL} />
-        </div>
-      )}
+      <ImageDisplay />
+      <ShareButton />
     </div>
   );
 };
 
-export default ImageDisplay;
+export default App;
--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { FacebookShareButton, TwitterShareButton, WhatsappShareButton } from 'react-share';
-import '../App.css'
+import '../App.css';
 
 const ShareButton = ({ imageURL }) => {
   const shareURL = `${window.location.href}?image=${encodeURIComponent(imageURL)}`;
 
   return (
     <div className="share-button">
-      <FacebookShareButton url={shareURL}>
-        <button className="facebook-share-button">Facebook</button>
+      <FacebookShareButton url={shareURL} quote="Check out this random image!">
+        <button className="facebook-share-button">Share on Facebook</button>
       </FacebookShareButton>
-      <TwitterShareButton url={shareURL}>
-        <button className="twitter-share-button">Twitter</button>
+      <TwitterShareButton url={shareURL} title="Random Image">
+        <button className="twitter-share-button">Share on Twitter</button>
       </TwitterShareButton>
-      <WhatsappShareButton url={shareURL}>
-        <button className="whatsapp-share-button">WhatsApp</button>
+      <WhatsappShareButton url={shareURL} title="Random Image">
+        <button className="whatsapp-share-button">Share on WhatsApp</button>
       </WhatsappShareButton>
     </div>
   );
