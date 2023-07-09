3069bb9d4185cccc67c1dc0dd20025edec542c74
Shrey Talreja <shreytalreja25@gmail.com>
2023-07-09 16:21:16 +0530

    Last Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 import ImageDisplay from './components/ImageDisplay';
 import ShareButton from './components/ShareButton';
+import './App.css';
 
 const App = () => {
   const [imageURL, setImageURL] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
-  const handleImageLoad = (url) => {
-    setImageURL(url);
-    updateMetaTag(url);
-  };
+  useEffect(() => {
+    fetchRandomImage();
+  }, []);
 
-  const updateMetaTag = (url) => {
-    const metaTag = document.querySelector('meta[property="og:image"]');
-    if (metaTag) {
-      metaTag.content = url;
+  const fetchRandomImage = async () => {
+    try {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await response.json();
+
+      if (data.status === 'success') {
+        setImageURL(data.message);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching random image:', error);
     }
   };
 
   return (
     <div className="App">
-      <ImageDisplay onLoad={handleImageLoad} />
-      {imageURL && <ShareButton imageURL={imageURL} />}
+      <Helmet>
+        <meta name="image" content={imageURL} />
+      </Helmet>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <ImageDisplay imageURL={imageURL} />
+          <ShareButton imageURL={imageURL} />
+        </div>
+      )}
     </div>
   );
 };
