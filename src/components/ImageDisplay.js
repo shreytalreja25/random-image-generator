5451566d41d04edbf9fa39791e4e8ff3e62f4e5b
Shrey Talreja <shreytalreja25@gmail.com>
2023-07-09 16:15:22 +0530

    tried adding meta in index.html

--- src/components/ImageDisplay.js
+++ src/components/ImageDisplay.js
@@ -7,33 +7,33 @@ const ImageDisplay = () => {
   const [imageURL, setImageURL] = useState('');
   const [isLoading, setIsLoading] = useState(true);
 
-  const fetchRandomImage = async () => {
-    try {
-      const response = await fetch('https://dog.ceo/api/breeds/image/random');
-      const data = await response.json();
-
-      if (data.status === 'success') {
-        setImageURL(data.message);
-
-        // Update the og:image meta tag
-        const metaTags = [
-          {
-            property: 'og:image',
-            content: data.message,
-          },
-        ];
-        Helmet.canUseDOM && Helmet.rewind();
-        Helmet.canUseDOM && Helmet.injectHelmetData({ metaTags });
-      }
-      setIsLoading(false);
-    } catch (error) {
-      console.error('Error fetching random image:', error);
-    }
-  };
-
   useEffect(() => {
+    const fetchRandomImage = async () => {
+      try {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        const data = await response.json();
+
+        if (data.status === 'success') {
+          setImageURL(data.message);
+
+          // Update the og:image meta tag
+          const metaTags = [
+            {
+              property: 'og:image',
+              content: data.message,
+            },
+          ];
+          Helmet.canUseDOM && Helmet.rewind();
+          Helmet.canUseDOM && Helmet.injectHelmetData({ metaTags });
+        }
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching random image:', error);
+      }
+    };
+
     fetchRandomImage();
-  }, [fetchRandomImage]);
+  }, []);
 
   return (
     <div className="App">
